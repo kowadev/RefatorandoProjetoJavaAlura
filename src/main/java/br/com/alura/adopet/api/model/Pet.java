162d236971df0ab306eb809fe80db5b859569340
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-09-15 23:29:52 -0300

    refatorando entidade Pet

--- src/main/java/br/com/alura/adopet/api/model/Pet.java
+++ src/main/java/br/com/alura/adopet/api/model/Pet.java
@@ -13,45 +13,41 @@ public class Pet {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    @NotNull
-    @Column(name = "tipo")
     private TipoPet tipo;
 
-    @NotBlank
-    @Column(name = "nome")
     private String nome;
 
-    @NotBlank
-    @Column(name = "raca")
     private String raca;
 
-    @NotNull
-    @Column(name = "idade")
     private Integer idade;
 
-    @NotBlank
-    @Column(name = "cor")
     private String cor;
 
-    @NotNull
-    @Column(name = "peso")
     private Float peso;
 
-    @Column(name = "adotado")
     private Boolean adotado;
 
-    @ManyToOne
-    @JsonBackReference("abrigo_pets")
-    @JoinColumn(name = "abrigo_id")
+    @ManyToOne(fetch = FetchType.LAZY)
     private Abrigo abrigo;
 
-    @OneToOne(mappedBy = "pet")
-    @JsonBackReference("adocao_pets")
+    @OneToOne(fetch = FetchType.LAZY)
     private Adocao adocao;
+
+    public Pet(){}
+
+    public Pet(Long id, TipoPet tipo, String nome, String raca, Integer idade, String cor, Float peso) {
+        this.id = id;
+        this.tipo = tipo;
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+        this.cor = cor;
+        this.peso = peso;
+        this.adotado = false;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -61,88 +57,43 @@ public class Pet {
         return Objects.equals(id, pet.id);
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public TipoPet getTipo() {
         return tipo;
     }
 
-    public void setTipo(TipoPet tipo) {
-        this.tipo = tipo;
+    public String getRaca() {
+        return raca;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getRaca() {
-        return raca;
-    }
-
-    public void setRaca(String raca) {
-        this.raca = raca;
-    }
-
     public Integer getIdade() {
         return idade;
-    }
-
-    public void setIdade(Integer idade) {
-        this.idade = idade;
     }
 
     public String getCor() {
         return cor;
     }
 
-    public void setCor(String cor) {
-        this.cor = cor;
+    public Boolean getAdotado() {
+        return adotado;
     }
 
     public Float getPeso() {
         return peso;
     }
 
-    public void setPeso(Float peso) {
-        this.peso = peso;
-    }
-
-    public Boolean getAdotado() {
-        return adotado;
-    }
-
-    public void setAdotado(Boolean adotado) {
-        this.adotado = adotado;
-    }
-
     public Abrigo getAbrigo() {
         return abrigo;
     }
 
-    public void setAbrigo(Abrigo abrigo) {
-        this.abrigo = abrigo;
-    }
-
     public Adocao getAdocao() {
         return adocao;
-    }
-
-    public void setAdocao(Adocao adocao) {
-        this.adocao = adocao;
     }
 }
